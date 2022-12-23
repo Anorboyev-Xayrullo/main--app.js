c18860ed05f792856a04214f8f9fd9447375a7c2
Anorboyev-Xayrullo <anorboyevxayrullo841@gmail.com>
2022-12-23 12:26:50 +0500

    commit

---
+++ app.js
@@ -0,0 +1,139 @@
+/*-----------------------------------------------------------------
+Sana: 16:53 22.12.2022
+
+Muallif:xayrullo   (DarkwerbCoders09)
+
+Masala:ayirmani toping
+
+------------------------------------------------------------------*/ 
+// 1 masala
+// function plus(a,b){
+//     return a*b
+// }
+// console.log(plus(6,7));
+
+
+// 2 masala
+// function plus (a){
+//     return a**2
+// }
+// console.log(plus(5))
+
+
+
+
+// function plus (a,b){
+//    return  a*b
+// }
+// console.log(plus(7,8))
+
+
+
+
+//4masala
+// function plus(a) {
+//     return a*5  
+// }
+// console.log(plus(6))
+
+
+// var a = 5;
+// var c =a++ + ++a +a++
+// console.log(c)
+
+// console.log(math.random );
+// var c =Math.random()*14 + 5
+// console.log(c);
+
+
+// var c = 13 && 2
+// console.log(c);
+// var a = 5
+// if(a < 6){
+//    console.log(true);
+// }else{
+//    console.log(false);
+// }
+// var summa = 1000
+// var pridmet = 1100
+// if(summa >= pridmet){
+//    console.log("men sotib olaman");
+// }else{
+//    console.log("sotib olmayman");
+// }
+// var teacher = false
+// if(education && teacher){
+//    console.log('dars boladi');
+// }else{
+//    console.log("dars bolmaydi");
+// }
+
+
+// var IELTS = 6
+// var myILTS = 6.5
+// var mytanka = false
+// if(myILTS >= IELTS ||tanka){
+//    console.log('kira olaman');
+// }else{
+//    console.log('kira olmayman');
+// }
+
+
+
+// var a = 0 && 6
+// console.log(a);
+
+
+
+// var a = 0 || 6
+// console.log(a);
+
+
+
+// // UZD
+// var flight = 500
+// var museum = 50
+
+// //EURO 
+// var hotel = 120
+// // 
+// var money = prompt("alisher qancha pulingiz bor")
+// if((flight + museum) *11000 + hotel *0.084<= money){
+//    console.log("oq yol alisher");
+// }else{
+//    console.log("ozgina sabr qil alisher");
+// }
+
+// var a = -5
+// if (a>0){
+//    console.log(a+1);
+// }else{
+//    console.log(a+2);
+// }
+// console.log(9%2);
+// var a = 0
+// if(a%2==0){
+//    console.log('juft son');
+// }else{
+//    console.log("toq son");
+// }
+
+
+// 
+// if (true || true&& false){
+//    console.log('hello');
+// }else{
+//    console.log('world');
+// }
+
+
+// if((true&& true) || (true && false)){
+//    console.log('hello');
+// }else{
+//    console.log('world');
+// }
+// if (true || 0 && true ){
+//    console.log('hello');
+// }else{
+//    console.log('world');
+// }
